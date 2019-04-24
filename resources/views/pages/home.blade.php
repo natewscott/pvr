8f50ea84dab28564635d8dcff2ed8793eea7e391
Nate Scott <scott.w.nate@gmail.com>
2019-04-24 10:20:15 -0700

    Changed cards

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,39 +10,39 @@
 		<h1>Paris Valley Ranch</h1>
 		<hr>
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda mollitia magnam at laudantium odit tenetur, amet optio deserunt esse, consequatur aspernatur veniam, quae accusantium animi similique. Reprehenderit dignissimos sint veritatis.
+			Paris Valley Ranch is a 1,000 Acre ranch nestled in the rolling hills of the Salinas Valley. The ranch consists of 3 grassy valleys, a full shooting range, and miles of trails perfect for quadding or dirt bike riding. Paris Valley Ranch has a main house, a bunkhouse and a “party pad” high on the hill overlooking the entire Salinas Valley. Whether you are looking to do some hunting, practice shooting, do some riding or just have a relaxing get away Paris Valley Ranch has it all.
 		</p>
 		<hr>
 	</article>
 	<article class="grid-cards">
 		<ul class="cards">
-			<li class="cards__item">
+			<li class="cards-item">
 				<div class="card">
-					<div class="card__image card__image--fence"></div>
-					<div class="card__content">
-						<div class="card__title">Flex</div>
-						<p class="card__text">This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
+					<div class="card-image card-image-main-house"></div>
+					<div class="card-content">
+						<div class="card-title">Flex</div>
+						<p class="card-text">This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
+						<button class="btn btn-block card-btn">Button</button>
+					</div>
+				</div>
+			</li>
+			<li class="cards-item">
+				<div class="card">
+					<div class="card-image card-image-range"></div>
+					<div class="card-content">
+						<div class="card-title">Flex Grow</div>
+						<p class="card-text">This defines the ability for a flex item to grow if necessary. It accepts a unitless value that serves as a proportion. It dictates what amount of the available space inside the flex container the item should take up.</p>
 						<button class="btn btn--block card__btn">Button</button>
 					</div>
 				</div>
 			</li>
-			<li class="cards__item">
+			<li class="cards-item">
 				<div class="card">
-					<div class="card__image card__image--river"></div>
-					<div class="card__content">
-						<div class="card__title">Flex Grow</div>
-						<p class="card__text">This defines the ability for a flex item to grow if necessary. It accepts a unitless value that serves as a proportion. It dictates what amount of the available space inside the flex container the item should take up.</p>
-						<button class="btn btn--block card__btn">Button</button>
-					</div>
-				</div>
-			</li>
-			<li class="cards__item">
-				<div class="card">
-					<div class="card__image card__image--record"></div>
-					<div class="card__content">
-						<div class="card__title">Flex Shrink</div>
-						<p class="card__text">This defines the ability for a flex item to shrink if necessary. Negative numbers are invalid.</p>
-						<button class="btn btn--block card__btn">Button</button>
+					<div class="card-image card-image-quads"></div>
+					<div class="card-content">
+						<div class="card-title">Flex Shrink</div>
+						<p class="card-text">This defines the ability for a flex item to shrink if necessary. Negative numbers are invalid.</p>
+						<button class="btn btn-block card-btn">Button</button>
 					</div>
 				</div>
 			</li>
