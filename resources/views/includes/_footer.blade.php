605077c1070ed55597d3782203ba51b87fbf4443
Nate Scott <scott.w.nate@gmail.com>
2019-04-24 15:44:36 -0700

    Started ranch page

--- resources/views/includes/_footer.blade.php
+++ resources/views/includes/_footer.blade.php
@@ -1,5 +1,7 @@
 <footer>
-	
+	<p>
+		Paris Valley Ranch | 2019
+	</p>
 </footer>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script>
--- resources/views/pages/ranch.blade.php
+++ resources/views/pages/ranch.blade.php
@@ -1,3 +1,13 @@
 @extends('main')
 @section('content')
+<section class="hero" id="ranch-hero">
+	<article>
+		
+	</article>
+</section>
+<section class="content">
+	<article class="inner-content">
+		
+	</article>
+</section>
 @stop
