fb524c25d154afd81e5dcded29386edec8195341
Nate Scott <scott.w.nate@gmail.com>
2019-11-15 09:34:46 -0800

    typo

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
-{{ $name }}
-{{ $lastname }}
-{{ $email }}
-{{ $subject }}
-{{ $msg }}
+{{ $request->$name }}
+{{ $request->$lastname }}
+{{ $request->$email }}
+{{ $request->$subject }}
+{{ $request->$msg }}
 @endcomponent
