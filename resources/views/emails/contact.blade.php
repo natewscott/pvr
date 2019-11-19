c3334905682deb01f9238edd7bd064813098e7dd
Nate Scott <scott.w.nate@gmail.com>
2019-11-19 14:32:56 -0800

    changed contact function

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,6 +1,11 @@
-
-{{ $name }}
-{{ $lastname }}
-{{ $email }}
-{{ $subject }}
-{{ $msg }}
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Contact Inquiry</title>
+</head>
+<body>
+	<img src="http://beta.pvrranch.com/img/misc/PVR%20-%20Lines.svg" alt="">
+	{{ $name}}
+	
+</body>
+</html>
