319bbcc1007db667c8e8fff6877db326f6bf9a3e
Nate Scott <scott.w.nate@gmail.com>
2020-01-31 09:03:10 -0800

    fixing contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,14 +2,40 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
-use App\Http\Request;
+use App\Http\Requests\NewContactRequest;
 use Mail;
+use App\Mail\NewContact;
 use Sessions;
 
 
 class ContactController extends Controller
 {
-   
+    public function show() {
+    	return view ('pages.contact');
+    }
+
+    public function mail(Request $request) {
+    	$this->validate($request, [
+    		"name" => 'required',
+            "lastname" => 'required',
+            "email" => 'required|email',
+            "subject" => 'required',
+            "msg" => 'required|min:10',
+    	]);
+    	
+    	Mail::send('emails.contact', [
+    		'name' => $request->name,
+            'lastname' => $request->lastname,
+            'email' => $request->email,
+            'subject' => $request->subject,
+            'msg' => $request->msg,
+    	],
+    	function($contact) use ($request) {
+    		$contact->from($request->email, $request->name);
+    		$contact->to('parisvalleyinfo@gmail.com')
+    				->subject('New Contact');
+    	});
+    	return redirect()->back()->with('success', 'Your email has benn sent.');
+    }
 }
