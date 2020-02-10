ea29b10ef0827590ad61cdee89ce6bfddb8c2266
Nate Scott <scott.w.nate@gmail.com>
2020-02-10 15:30:50 -0800

    added about content

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -8,7 +8,7 @@
 <section class="content">
 	<article class="inner-content">
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure dolore delectus magnam voluptate mollitia, fuga blanditiis at quia natus adipisci. Nihil praesentium quae, eum a dignissimos alias voluptates voluptate molestias.
+			Paris Valley Ranch was founded in 2016 by the Mack Family. The Ranch sites on 1,000 acres in the rolling hills of the San Ardo CA. The Ranch was founded with the intent of giving providing it's guest with world class aminities, but allow you to be completely removed from the modern world. Weither you are looking for a quick realxing weekend get away or a chance to do some hunt Paris Valley Ranch has it all for you.
 		</p>
 	</article>
 	<hr>
@@ -20,7 +20,7 @@
 					<div class="card-content">
 						<div class="card-title">Reed Mack</div>
 						<div class="card-price">Owner</div>
-						<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi officiis ea dignissimos, exercitationem aut corporis dolore sunt debitis expedita illo voluptatibus aliquid cumque voluptas, itaque vel! Voluptatum provident, laboriosam rem?</p>
+						{{-- <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi officiis ea dignissimos, exercitationem aut corporis dolore sunt debitis expedita illo voluptatibus aliquid cumque voluptas, itaque vel! Voluptatum provident, laboriosam rem?</p> --}}
 					</div>
 				</div>
 			</li>
@@ -30,7 +30,7 @@
 					<div class="card-content">
 						<div class="card-title">Spencer Keiser</div>
 						<div class="card-price">Facilities Manager</div>
-						<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis molestiae, enim consectetur iste cum aperiam facere, quasi nobis ea numquam dolores error facilis quia velit optio saepe, animi minima sed.</p>
+						{{-- <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis molestiae, enim consectetur iste cum aperiam facere, quasi nobis ea numquam dolores error facilis quia velit optio saepe, animi minima sed.</p> --}}
 					</div>
 				</div>
 			</li>
